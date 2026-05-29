cfded90a070f4786baff6bbb011d94b2cbb843ef
Kings-Maen <2425401075@live.gctu.edu.gh>
2026-05-29 21:52:33 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Klinsman Nana Owusu
+# STUDENT ID: 2425401075
+# Email: 2425401075@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Klinsman Nana Owusu, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
+    # This program prints my initials in stylized large letters
 
+    print()
+    print("...KNO")
+    print()
+
+print("...KKKK....KKKK........NNNN....NNNN........OOOOOOOOOOOO.....")
+print("...KKKK...KKKK.........NNNNN...NNNN........OOOOOOOOOOOO.....")
+print("...KKKK..KKKK..........NNNNN...NNNN........OOOO....OOOO.....")
+print("...KKKK.KKK............NNNNNN..NNNN........OOOO....OOOO.....")
+print("...KKKK.KKK............NNNNNNN.NNNN........OOOO....OOOO.....")
+print("...KKKK.KKKK...........NNNN.NNNNNNN........OOOO....OOOO.....")
+print("...KKKK..KKKK..........NNNN..NNNNNN........OOOO....OOOO.....")
+print("...KKKK...KKKK.........NNNN...NNNNN........OOOO....OOOO.....")
+print("...KKKK....KKKK...**...NNNN...NNNNN...**...OOOOOOOOOOOO...**")
+print("...KKKK....KKKK...**...NNNN....NNNN...**...OOOOOOOOOOOO...**")
+print()
 
 
 main()
